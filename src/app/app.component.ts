f340dc74be7331e893030a214bac9980d2db5e0a
AhmadRiaz0 <101810743+AhmadRiaz0@users.noreply.github.com>
2025-04-04 09:45:11 +0200

    Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from "./user/user.component"; //3.Importer UserComponent dans AppComponent
-import { DUMMY_USERS } from "./user/dummy-users"; //5. Importer les utilisateur
+import { UserComponent } from "./user/user.component"; //Importer UserComponent dans AppComponent
+import { DUMMY_USERS } from "./user/dummy-users"; //Importer les utilisateur
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent], //4.On ajoute UserComponent dans imports, ce qui permet d'afficher des utilisateurs dynamiquement.
-
-
+  imports: [HeaderComponent, UserComponent], //On ajoute UserComponent dans imports, ce qui permet d'afficher des utilisateurs dynamiquement.
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  users = DUMMY_USERS; //6. Rajouter une propriété pour stocker les utilisateurs
+  users = DUMMY_USERS; //Rajouter une propriété pour stocker les utilisateurs
+
+  onSelectUser(id: string) { //4. Permet au parent de récupérer les données envoyées par l'enfant, et decide de l'action a adopter
+    console.log('Selected user with id ' + id);
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,8 @@
 import { Component, computed } from '@angular/core';
-import { Input } from '@angular/core'; //1. Décorateur @Input(), qui sert à recevoir des données depuis un autre composant parent.
-import { input } from '@angular/core'; //2. Fonction input, permet de transformer la propriété en fonction + on utilise le mecanisme de Signal pour la gestion des changements sur cette propriété
+import { Input } from '@angular/core'; //Décorateur @Input(), qui sert à recevoir des données depuis un autre composant parent.
+import { input } from '@angular/core'; //Fonction input, permet de transformer la propriété en fonction + on utilise le mecanisme de Signal pour la gestion des changements sur cette propriété
+import { Output } from '@angular/core'; //Fonction output, permet de d'envoyer des événements d'un composant enfant vers un composant parent. 
+import { EventEmitter } from '@angular/core'; //Objet EventEmitter, permet de transmettre des données lorsqu'un événement se produit dans le composant enfant. 
 
 @Component({
   selector: 'app-user',
@@ -12,22 +14,26 @@ import { input } from '@angular/core'; //2. Fonction input, permet de transforme
 //Toutes les propriete défini dans cette classe sont accessible dans le fichier component.html 
 export class UserComponent {
   //Angular Decorateur @input
- @Input({required: true}) avatar!: string; //2.Décorateur @Input() aux propriétés du composant (Ces valeurs seront envoyées par un autre composant (le composant parent)), "required" : force le parent à fournir une valeur obligatoire à l'@Input
- @Input({required: true}) name!: string; //2.Décorateur @Input() aux propriétés du composant (Ces valeurs seront envoyées par un autre composant (le composant parent)), "required" : force le parent à fournir une valeur obligatoire à l'@Input
+ @Input({required: true}) id!: string; //Décorateur @Input() aux propriétés du composant (Ces valeurs seront envoyées par un autre composant (le composant parent)), "required" : force le parent à fournir une valeur obligatoire à l'@Input
+ @Input({required: true}) avatar!: string; //Décorateur @Input() aux propriétés du composant (Ces valeurs seront envoyées par un autre composant (le composant parent)), "required" : force le parent à fournir une valeur obligatoire à l'@Input
+ @Input({required: true}) name!: string; //Décorateur @Input() aux propriétés du composant (Ces valeurs seront envoyées par un autre composant (le composant parent)), "required" : force le parent à fournir une valeur obligatoire à l'@Input
+ @Output() select = new EventEmitter(); //Objet EventEmitter, envoyer des événements d'un composant enfant vers un composant parent. 
+
 
  get imagePath(){
-  return 'assets/users/users/' + this.avatar; //9. Cela permet de générer le chemin de l'image dynamiquement en fonction du avatar reçu.
+  return 'assets/users/users/' + this.avatar; //Cela permet de générer le chemin de l'image dynamiquement en fonction du avatar reçu.
  }
 
 
 
 onSelectUser(){
+  this.select.emit(this.id); //2. Méthode venant de EventEmitter, permet de transmettre une nouvelle valeur vers le composant parent
 }
 
 
 //Angular 17 : fonction input
-//avatar = input.required<string>() //3.Fonction input(), on transforme la propriété en fonction + on utilise le mecanisme de Signal pour la gestion des changements sur cette propriété
-//name = input.required<string>() //3.Fonction input(), on transforme la propriété en fonction + on utilise le mecanisme de Signal pour la gestion des changements sur cette propriété
+//avatar = input.required<string>() //Fonction input(), on transforme la propriété en fonction + on utilise le mecanisme de Signal pour la gestion des changements sur cette propriété
+//name = input.required<string>() //Fonction input(), on transforme la propriété en fonction + on utilise le mecanisme de Signal pour la gestion des changements sur cette propriété
 //imagePath = computed(() => {
 //  return 'assets/users/users/' + this.avatar;
 //})
